bc268e946247e44a3bfabba1f3ee5ca873887903
scottmckendry <39483124+scottmckendry@users.noreply.github.com>
2025-10-09 00:29:08 +0000

    chore: bump zen-browser version to 1.16.4b

--- flake.nix
+++ flake.nix
@@ -8,15 +8,15 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      version = "1.16.3b";
+      version = "1.16.4b";
       downloadUrl = {
         "specific" = {
 	  url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.xz";
-	  sha256 = "sha256:15a71a7f0kn3b6j4c6nhma1nlicr179dzgr0ncdnbwvng7yfyn87";
+	  sha256 = "sha256:1m2ba5011km8mrj3vxh991bypxqdgl5m02ab78p1rg8x1c45vjqc";
 	};
 	"generic" = {
 	  url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.xz";
-	  sha256 = "sha256:15a71a7f0kn3b6j4c6nhma1nlicr179dzgr0ncdnbwvng7yfyn87";
+	  sha256 = "sha256:1m2ba5011km8mrj3vxh991bypxqdgl5m02ab78p1rg8x1c45vjqc";
 	};
       };
 
